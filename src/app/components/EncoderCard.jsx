95c79444cd0e9e173d0473098369a451545276bc
tecnopulsar <tecnopulsar@gmail.com>
2024-09-17 09:12:30 -0300

    update img type

--- src/app/components/EncoderCard.jsx
+++ src/app/components/EncoderCard.jsx
@@ -3,32 +3,50 @@
 import { useEffect, useState } from 'react';
 
 export default function EncoderCard() {
-  const [imageSrc, setImageSrc] = useState(``);
+  const [imageSrc, setImageSrc] = useState('');
 
   useEffect(() => {
     const fetchImage = async () => {
       try {
         // Descargar la imagen desde el servidor
+        // const response = await fetch(`/preview/1001/preview`);
         const response = await fetch(`http://192.168.16.45/api/v2/public/common/devices/Source3-PCShow/preview`);
 
         if (!response.ok) {
           throw new Error(`Error fetching image: ${response.statusText}`);
         }
-        console.log(response.url);
-        // console.log(JSON.stringify(response));
 
+        // Convertir la respuesta en un blob con el tipo MIME explícito 'image/jpeg'
+        const blob = await response.blob();
 
-        setImageSrc(response.url);
+        // Verificar si el Blob es de tipo 'image/jpeg'
+        // const imageType = blob.type === 'image/jpeg' ? 'jpeg' : 'jpeg'; // Asumimos que es JPEG
+
+        // Crear un objeto URL para la imagen blob
+        const imageBlobURL = URL.createObjectURL(new Blob([blob], { type: 'image/jpeg' }));
+
+        // Guardar la imagen en localStorage con una extensión .jpeg
+        localStorage.setItem('cachedImage', imageBlobURL);
+
+        // Actualizar la fuente de la imagen
+        setImageSrc(imageBlobURL);
+
       } catch (error) {
         console.error('Error fetching the preview image:', error);
       }
     };
 
+    // Verificar si hay una imagen previamente almacenada
+    const cachedImage = localStorage.getItem('cachedImage');
+    if (cachedImage) {
+      setImageSrc(cachedImage);
+    } else {
+      // Si no hay imagen almacenada, la descargamos
+      fetchImage();
+    }
+
     // Configurar el intervalo para actualizar la imagen cada 20 segundos
     const interval = setInterval(fetchImage, 20000);
-
-    // Llamar a la función para la primera carga
-    fetchImage();
 
     // Limpiar el intervalo al desmontar el componente
     return () => clearInterval(interval);
@@ -36,8 +54,12 @@ export default function EncoderCard() {
 
   return (
     <div>
-      <h2>Preview for Decoder {decoderId}</h2>
-      <img src={imageSrc} alt={"Preview for decoder"} width={240} height={135} />
+      <h2>Preview for Decoder</h2>
+      {imageSrc ? (
+        <img src={imageSrc} alt="Preview for decoder" width={240} height={135} />
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
