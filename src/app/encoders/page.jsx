38d386a58d1e0d8a31e9dcb728555f5e1ee32c2d
tecnopulsar <tecnopulsar@gmail.com>
2024-09-18 16:02:42 -0300

    test velocidad deploy

--- src/app/encoders/page.jsx
+++ src/app/encoders/page.jsx
@@ -3,9 +3,9 @@ import EncoderCard from "../components/EncoderCard";
 export default function EncodersPage() {
     return (
         <div>
-            <h1>Encoders Page</h1>
-            <EncoderCard nameDevice='Alem1'/>
-            <EncoderCard nameDevice='Alem2'/>
+            <h1>Encoders Preview</h1>
+            <EncoderCard nameDevice='Alem1' />
+            <EncoderCard nameDevice='Alem2' />
         </div>
     );
 }
